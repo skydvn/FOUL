72b8e0816bd81ed4a9041ab9daebd4ae126b0bee
skydvn <mduongbkhn@gmail.com>
2024-11-08 13:23:04 +0900

    pogm

--- system/flcore/servers/serverfoul.py
+++ system/flcore/servers/serverfoul.py
@@ -18,6 +18,7 @@
 import time
 from flcore.clients.clientfoul import clientFOUL
 from flcore.servers.serverbase import Server
+from utils.model_utils import ParamDict
 from threading import Thread
 import torch
 import copy
@@ -131,7 +132,13 @@ class FOUL(Server):
                 self.call_dlg(i)
 
             # self.aggregate_parameters()
-            self.aggregate_foul()
+            # self.aggregate_foul()
+            meta_weights = self.aggregate_foul(
+                meta_weights=self.global_model,
+                inner_weights=self.selected_clients, # This one should be list of models 
+                lr_meta=self.hparams["meta_lr"]
+            )
+            self.network.reset_weights(meta_weights)
 
             self.Budget.append(time.time() - s_t)
             print('-' * 25, 'time cost', '-' * 25, self.Budget[-1])
@@ -141,11 +148,6 @@ class FOUL(Server):
 
         print("\nBest accuracy.")
         # self.print_(max(self.rs_test_acc), max(self.cagrad_c = args.c_parameter
-        self.cagrad_rounds = args.cagrad_rounds
-        self.cagrad_learning_rate = args.cagrad_learning_rate
-        self.momentum = args.momentum
-        self.step_size = args.step_size
-        self.gamma = args.gamma
         #     self.rs_train_acc), min(self.rs_train_loss))
         print(max(self.rs_test_acc))
         print("\nAverage time cost per round.")
@@ -161,116 +163,144 @@ class FOUL(Server):
             print("\nEvaluate new clients")
             self.evaluate()
 
-    def aggregate_foul(self):
-        grad_ez = sum(p.numel() for p in self.global_model.parameters())
-        grads = torch.Tensor(grad_ez, self.num_clients)
+    def cag(self, meta_weights, inner_weights, lr_meta):
 
-        for index, model in enumerate(self.grads):
-            grad2vec2(model, grads, index)
+        # Lấy tất cả parameter names
+        param_names = [name for name, _ in meta_weights.named_parameters()]
 
-        # Split to grads retain & grads forget
-        g = self.cagrad(grads, self.num_clients)
+        # Tính toán gradient chênh lệch cho mỗi domain
+        domain_grad_diffs = []
+        for i_domain in range(self.client):
+            domain_grads = []
+            for (clone_param, meta_param, name) in zip(inner_weights[i_domain].parameters(), meta_weights.parameters(),
+                                                       param_names):
+                domain_grads.append(torch.zeros_like(torch.flatten(meta_param)))
+            domain_grad_diffs.append(torch.cat(domain_grads))
 
-        model_origin = copy.deepcopy(self.global_model)
-        self.overwrite_grad2(self.global_model, g)
-        for param in self.global_model.parameters():
-            param.data += param.grad
+        all_domains_grad_tensor = torch.stack(domain_grad_diffs)
+        # print(all_domains_grad_tensor)
+        cagrad = self.cagrad(all_domains_grad_tensor, self.num_domains)
+        # print(cagrad)
 
-        angle = [self.cos_sim(model_origin, self.global_model, models) for models in self.grads]
-        self.angle_value = statistics.mean(angle)
+        # Cập nhật trọng số meta
+        meta_weights_vector = parameters_to_vector(meta_weights.parameters())
+        vector_to_parameters(meta_weights_vector + cagrad * lr_meta, meta_weights.parameters())
 
-        angle_value = []
-        for i in self.grads:
-            for j in self.grads:
-                angle_value = [self.cosine_similarity(i, j)]
+        # Tạo và in ra ParamDict mới từ trạng thái cập nhật của meta_weights
+        updated_meta_weights = ParamDict(meta_weights.state_dict())
 
-        self.grads_angle_value = statistics.mean(angle_value)
+        return updated_meta_weights
 
-    def cagrad(self, grad_vec, num_tasks):
-
-        grads = grad_vec.to(self.device)
-
-        GG = grads.t().mm(grads)
-        # to(device)
-        scale = (torch.diag(GG) + 1e-4).sqrt().mean()
-        GG = GG / scale.pow(2)
-        Gg = GG.mean(1, keepdims=True)
-        gg = Gg.mean(0, keepdims=True)
-
-        w = torch.zeros(num_tasks, 1, requires_grad=True, device=self.device)
-        #         w = torch.zeros(num_tasks, 1, requires_grad=True).to(self.device)
-
-        if num_tasks == 50:
-            w_opt = torch.optim.SGD([w], lr=self.cagrad_learning_rate * 2, momentum=self.momentum)
-        else:
-            w_opt = torch.optim.SGD([w], lr=self.cagrad_learning_rate, momentum=self.momentum)
-
-        scheduler = StepLR(w_opt, step_size=self.step_size, gamma=self.gamma)
-
-        c = (gg + 1e-4).sqrt() * self.cagrad_c
-        w_best = None
-        obj_best = np.inf
-        for i in range(self.cagrad_rounds + 1):
-            w_opt.zero_grad()
-            ww = torch.softmax(w, dim=0)
-            obj = ww.t().mm(Gg) + c * (ww.t().mm(GG).mm(ww) + 1e-4).sqrt()
-            if obj.item() < obj_best:
-                obj_best = obj.item()
-                w_best = w.clone()
-            if i < self.cagrad_rounds:
-                obj.backward()
-                w_opt.step()
-                scheduler.step()
-
-                # Check this scheduler. step()
-
-        ww = torch.softmax(w_best, dim=0)
-        gw_norm = (ww.t().mm(GG).mm(ww) + 1e-4).sqrt()
-
-        lmbda = c.view(-1) / (gw_norm + 1e-4)
-        g = ((1 / num_tasks + ww * lmbda).view(
-            -1, 1).to(grads.device) * grads.t()).sum(0) / (1 + self.cagrad_c ** 2)
-        return g
-
-        # def overwrite_grad(self, m, newgrad, grad_dims):
-        #     newgrad = newgrad * self.num_clients  # to match the sum loss
-        #     cnt = 0
-        #     for mm in m.shared_modules():
-        #         for param in mm.parameters():
-        #             beg = 0 if cnt == 0 else sum(grad_dims[:cnt])
-        #             en = sum(grad_dims[:cnt + 1])
-        #             this_grad = newgrad[beg: en].contiguous().view(param.data.size())
-        #             param.grad = this_grad.data.clone()
-        #             cnt += 1
-
-    def overwrite_grad2(self, m, newgrad):
-        newgrad = newgrad * self.num_clients
-        for param in m.parameters():
-            # Get the number of elements in the current parameter
-            num_elements = param.numel()
-
-            # Extract a slice of new_params with the same number of elements
-            param_slice = newgrad[:num_elements]
-
-            # Reshape the slice to match the shape of the current parameter
-            param.grad = param_slice.view(param.data.size())
-
-            # Move to the next slice in new_params
-            newgrad = newgrad[num_elements:]
-
-    # def grad2vec(m, grads, grad_dims, task):
-    #     grads[:, task].fill_(0.0)
-    #     cnt = 0
-    #     for mm in m.shared_modules():
-    #         for p in mm.parameters():
-    #             grad_cur = p.data.detach().clone()
-    #             beg = 0 if cnt == 0 else sum(grad_dims[:cnt])
-    #             en = sum(grad_dims[:cnt + 1])
-    #             grads[beg:en, task].copy_(grad_cur.data.view(-1))
-    #             cnt += 1
-
-def grad2vec2(m, grads, task):
-    grads[:, task].fill_(0.0)
-    all_params = torch.cat([param.detach().view(-1) for param in m.parameters()])
-    # print(all_params.size())
-    grads[:, task].copy_(all_params)
+#     def aggregate_foul(self):
+#         grad_ez = sum(p.numel() for p in self.global_model.parameters())
+#         grads = torch.Tensor(grad_ez, self.num_clients)
+#
+#         for index, model in enumerate(self.grads):
+#             grad2vec2(model, grads, index)
+#
+#         # Split to grads retain & grads forget
+#         g = self.cagrad(grads, self.num_clients)
+#
+#         model_origin = copy.deepcopy(self.global_model)
+#         self.overwrite_grad2(self.global_model, g)
+#         for param in self.global_model.parameters():
+#             param.data += param.grad
+#
+#         angle = [self.cos_sim(model_origin, self.global_model, models) for models in self.grads]
+#         self.angle_value = statistics.mean(angle)
+#
+#         angle_value = []
+#         for i in self.grads:
+#             for j in self.grads:
+#                 angle_value = [self.cosine_similarity(i, j)]
+#
+#         self.grads_angle_value = statistics.mean(angle_value)
+#
+#     def cagrad(self, grad_vec, num_tasks):
+#
+#         grads = grad_vec.to(self.device)
+#
+#         GG = grads.t().mm(grads)
+#         # to(device)
+#         scale = (torch.diag(GG) + 1e-4).sqrt().mean()
+#         GG = GG / scale.pow(2)
+#         Gg = GG.mean(1, keepdims=True)
+#         gg = Gg.mean(0, keepdims=True)
+#
+#         w = torch.zeros(num_tasks, 1, requires_grad=True, device=self.device)
+#         #         w = torch.zeros(num_tasks, 1, requires_grad=True).to(self.device)
+#
+#         if num_tasks == 50:
+#             w_opt = torch.optim.SGD([w], lr=self.cagrad_learning_rate * 2, momentum=self.momentum)
+#         else:
+#             w_opt = torch.optim.SGD([w], lr=self.cagrad_learning_rate, momentum=self.momentum)
+#
+#         scheduler = StepLR(w_opt, step_size=self.step_size, gamma=self.gamma)
+#
+#         c = (gg + 1e-4).sqrt() * self.cagrad_c
+#         w_best = None
+#         obj_best = np.inf
+#         for i in range(self.cagrad_rounds + 1):
+#             w_opt.zero_grad()
+#             ww = torch.softmax(w, dim=0)
+#             obj = ww.t().mm(Gg) + c * (ww.t().mm(GG).mm(ww) + 1e-4).sqrt()
+#             if obj.item() < obj_best:
+#                 obj_best = obj.item()
+#                 w_best = w.clone()
+#             if i < self.cagrad_rounds:
+#                 obj.backward()
+#                 w_opt.step()
+#                 scheduler.step()
+#
+#                 # Check this scheduler. step()
+#
+#         ww = torch.softmax(w_best, dim=0)
+#         gw_norm = (ww.t().mm(GG).mm(ww) + 1e-4).sqrt()
+#
+#         lmbda = c.view(-1) / (gw_norm + 1e-4)
+#         g = ((1 / num_tasks + ww * lmbda).view(
+#             -1, 1).to(grads.device) * grads.t()).sum(0) / (1 + self.cagrad_c ** 2)
+#         return g
+#
+#         # def overwrite_grad(self, m, newgrad, grad_dims):
+#         #     newgrad = newgrad * self.num_clients  # to match the sum loss
+#         #     cnt = 0
+#         #     for mm in m.shared_modules():
+#         #         for param in mm.parameters():
+#         #             beg = 0 if cnt == 0 else sum(grad_dims[:cnt])
+#         #             en = sum(grad_dims[:cnt + 1])
+#         #             this_grad = newgrad[beg: en].contiguous().view(param.data.size())
+#         #             param.grad = this_grad.data.clone()
+#         #             cnt += 1
+#
+#     def overwrite_grad2(self, m, newgrad):
+#         newgrad = newgrad * self.num_clients
+#         for param in m.parameters():
+#             # Get the number of elements in the current parameter
+#             num_elements = param.numel()
+#
+#             # Extract a slice of new_params with the same number of elements
+#             param_slice = newgrad[:num_elements]
+#
+#             # Reshape the slice to match the shape of the current parameter
+#             param.grad = param_slice.view(param.data.size())
+#
+#             # Move to the next slice in new_params
+#             newgrad = newgrad[num_elements:]
+#
+#     # def grad2vec(m, grads, grad_dims, task):
+#     #     grads[:, task].fill_(0.0)
+#     #     cnt = 0
+#     #     for mm in m.shared_modules():
+#     #         for p in mm.parameters():
+#     #             grad_cur = p.data.detach().clone()
+#     #             beg = 0 if cnt == 0 else sum(grad_dims[:cnt])
+#     #             en = sum(grad_dims[:cnt + 1])
+#     #             grads[beg:en, task].copy_(grad_cur.data.view(-1))
+#     #             cnt += 1
+#
+# def grad2vec2(m, grads, task):
+#     grads[:, task].fill_(0.0)
+#     all_params = torch.cat([param.detach().view(-1) for param in m.parameters()])
+#     # print(all_params.size())
+#     grads[:, task].copy_(all_params)
